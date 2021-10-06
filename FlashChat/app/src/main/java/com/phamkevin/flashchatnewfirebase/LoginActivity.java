7407db41252b2576c3d8114a9e82de91560cfb58
Kevin Pham <phambamkevin@gmail.com>
2021-10-06 14:00:52 -0700

    Register new users

--- FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/LoginActivity.java
+++ FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/LoginActivity.java
@@ -2,13 +2,14 @@ package com.phamkevin.flashchatnewfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
--- FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/RegisterActivity.java
+++ FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/RegisterActivity.java
@@ -1,14 +1,23 @@
 package com.phamkevin.flashchatnewfirebase;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -25,7 +34,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mConfirmPasswordView;
 
     // Firebase instance variables
-
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -51,6 +60,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         // TODO: Get hold of an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
 
     }
@@ -97,6 +107,7 @@ public class RegisterActivity extends AppCompatActivity {
             focusView.requestFocus();
         } else {
             // TODO: Call create FirebaseUser() here
+            createFirebaseUser();
 
         }
     }
@@ -108,17 +119,40 @@ public class RegisterActivity extends AppCompatActivity {
 
     private boolean isPasswordValid(String password) {
         //TODO: Add own logic to check for a valid password (minimum 6 characters)
-        return true;
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length() > 6;
     }
 
     // TODO: Create a Firebase user
+    private void createFirebaseUser() {
+        String email = mEmailView.getText().toString();
+        String password =  mPasswordView.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "createUser onComplete: " + task.isSuccessful());
 
+                if(!task.isSuccessful()) {
+                    Log.d("FlashChat", "user creation failed");
+                    showErrorDialog("Registration attempt failed");
+                }
+            }
+        });
+    }
 
     // TODO: Save the display name to Shared Preferences
-
+    
 
     // TODO: Create an alert dialog to show in case registration failed
+    private void showErrorDialog(String message) {
 
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 
 
