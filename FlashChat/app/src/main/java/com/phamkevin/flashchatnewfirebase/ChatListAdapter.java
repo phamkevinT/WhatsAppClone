3b8f229ade1e30c25da92c4dabb75c54e87865a2
Kevin Pham <phambamkevin@gmail.com>
2021-10-07 19:44:24 -0700

    Final Commit - Text Bubbles arent aligned correctly

--- FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/ChatListAdapter.java
+++ FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/ChatListAdapter.java
@@ -1,14 +1,26 @@
 package com.phamkevin.flashchatnewfirebase;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -19,11 +31,42 @@ public class ChatListAdapter extends BaseAdapter {
     private String mDisplayName;
     private ArrayList<DataSnapshot> mSnapshotList;
 
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+            mSnapshotList.add(dataSnapshot);
+            notifyDataSetChanged();
+
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
+
 
     public ChatListAdapter(Activity activity, DatabaseReference ref, String name) {
         mActivity = activity;
         mDisplayName = name;
-        mDatabaseReference = ref.child("message");
+        mDatabaseReference = ref.child("messages");
+        mDatabaseReference.addChildEventListener(mListener);
+
         mSnapshotList = new ArrayList<>();
     }
 
@@ -35,21 +78,72 @@ public class ChatListAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return mSnapshotList.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public InstantMessage getItem(int position) {
+
+        DataSnapshot snapshot = mSnapshotList.get(position);
+        return snapshot.getValue(InstantMessage.class);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.chat_msg_row, parent, false);
+            final ViewHolder holder = new ViewHolder();
+            holder.authorName = (TextView) convertView.findViewById(R.id.author);
+            holder.body = (TextView) convertView.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+            convertView.setTag(holder);
+        }
+
+        final InstantMessage message = getItem(position);
+        final ViewHolder holder = (ViewHolder) convertView.getTag();
+
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+        setChatRowAppearance(isMe, holder);
+
+        String author = message.getAuthor();
+        holder.authorName.setText(author);
+
+        String msg = message.getMessage();
+        holder.body.setText(msg);
+
+        return convertView;
     }
+
+    private void setChatRowAppearance(boolean isItMe, ViewHolder holder) {
+
+        if (isItMe) {
+
+            holder.body.setGravity(Gravity.END);
+//            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.GREEN);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        } else {
+            holder.body.setGravity(Gravity.START);
+//            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }
+
+        holder.authorName.setLayoutParams(holder.params);
+        holder.body.setLayoutParams(holder.params);
+    }
+
+
+    public void cleanup() {
+        mDatabaseReference.removeEventListener(mListener);
+    }
+
+
 }
--- FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/MainChatActivity.java
+++ FlashChat/app/src/main/java/com/phamkevin/flashchatnewfirebase/MainChatActivity.java
@@ -25,6 +25,7 @@ public class MainChatActivity extends AppCompatActivity {
     private EditText mInputText;
     private ImageButton mSendButton;
     private DatabaseReference mDatabaseReference;
+    private ChatListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +88,12 @@ public class MainChatActivity extends AppCompatActivity {
 
 
     // TODO: Override the onStart() lifecycle method. Setup the adapter here.
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAdapter = new ChatListAdapter(this, mDatabaseReference, mDisplayName);
+        mChatListView.setAdapter(mAdapter);
+    }
 
 
     @Override
@@ -95,7 +101,7 @@ public class MainChatActivity extends AppCompatActivity {
         super.onStop();
 
         // TODO: Remove the Firebase event listener on the adapter.
-
+        mAdapter.cleanup();
     }
 
 }
